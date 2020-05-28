a2f4d6df556e5b4077cf387d993300da46c334f1
Franck Formis <franck.formis@gmail.com>
2020-05-28 23:16:21 +0200

    v3

--- bissextile.py
+++ bissextile.py
@@ -1,10 +1,15 @@
-# Programme testant si une année, saisie par l'utilisateur, est bissextile ou non
-# -*-coding:Latin-1 -*
+# Programme testant si une annÃ©e, saisie par l'utilisateur, est bissextile ou non
+# -*-coding:utf-8 -*
 
-annee = input("Saisissez une année : ") # On attend que l'utilisateur fournisse l'année qu'il désire tester
-annee = int(annee) # Risque d'erreur si l'utilisateur n'a pas saisi un nombre
-
-if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
-    print("L'année choisie est bissextile.")
-else:
-    print("L'année choisie n'est pas bissextile.")
+annee = input("Saisissez une annÃ©e : ") # On attend que l'utilisateur fournisse l'annÃ©e qu'il dÃ©sire tester
+try:
+    annee = int(annee)
+    if annee<=0:
+        raise ValueError("l'annÃ©e saisie est nÃ©gative ou nulle")
+except ValueError:
+    print("La valeur saisie est invalide (l'annÃ©e est peut-Ãªtre nÃ©gative).")
+else:    
+    if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
+        print("L'annÃ©e choisie est bissextile.")
+    else:
+        print("L'annÃ©e choisie n'est pas bissextile.")
